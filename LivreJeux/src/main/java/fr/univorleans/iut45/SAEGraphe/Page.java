c140a7fe0e27ef84fef9fb2f9de0298f96929b3a
Boulet--Chopinet Aymeric <o22503050@or-iut-i203-f23.iut45.univ-orleans.fr>
2026-04-28 14:22:59 +0200

    Modif de pom et création du fichier Page(attribue et constructeur)

---
+++ LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/Page.java
@@ -0,0 +1,16 @@
+package fr.univorleans.iut45.SAEGraphe;
+
+public class Page{
+
+    private int numero;
+    private boolean fin;
+    private String texte;
+    private String enigme;
+
+    public Page(int numero, boolean fin, String texte, String enigme){
+        this.numero = numero;
+        this.fin = fin ;
+        this.texte = texte;
+        this.enigme = enigme;
+    }
+}
