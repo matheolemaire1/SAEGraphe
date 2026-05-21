83f8d7503c8911c1ec3008e9e9569b88ba10d60d
Remi-Bens <remi.bens@etu.univ-orleans.fr>
2026-05-21 15:29:26 +0200

    Mise à jour du générateur de graphes

--- LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/GenerateurGraphe.java
+++ LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/GenerateurGraphe.java
@@ -16,7 +16,11 @@ public class GenerateurGraphe {
 
     private Random choix = new Random();
 
+
+    private Random choix = new Random();
+
     public Graph<Page,DefaultEdge> creation(int ordre,int nbObjets,List<Page> pages){
+
 
         Graph<Page,DefaultEdge> graphe = nuageDePoints(ordre, pages);
 
@@ -48,12 +52,41 @@ public class GenerateurGraphe {
         }
 
 
+
+        randomPage(new ArrayList<>(graphe.vertexSet())).setDeb(); //on met une carte aléatoire en début
+
+        Page fin = randomPage(new ArrayList<>(graphe.vertexSet()));
+
+        if (!fin.getDeb()) fin.setFin();
+
+        for (Page page:graphe.vertexSet()) {
+            
+            if (!page.getFin()){    
+                int tentatives = 0;
+                while ((page.getNbReponses()>graphe.outDegreeOf(page)) && tentatives<100){
+                    try {
+                        boolean ajoute = graphe.addEdge(page,randomPageJoinable(new ArrayList<>(graphe.vertexSet())));
+
+                        if (!ajoute) tentatives+=1;
+                        else tentatives = 0;
+                    } catch (NoJoinablePageException e){
+                        System.err.println("Erreur: "+e.getMessage());
+                        break;
+                    }
+                    
+                }
+                
+
+            }
+        }
+
+
         return graphe;
 
     }
 
     private Graph<Page,DefaultEdge> nuageDePoints(int ordre,List<Page> pages){
-        Graph<Page,DefaultEdge> graphe = new DefaultDirectedGraph<>(DefaultEdge.class);
+        Graph<Page,DefaultEdge> graphe = new DefaultDirectedGraph<>(DefaultEdge.classDefaultEdge.class);
 
         while (graphe.vertexSet().size() < ordre) {
             graphe.addVertex(randomPage(pages));
@@ -85,6 +118,33 @@ public class GenerateurGraphe {
         }
         throw new NoJoinablePageException("La liste est vide");
         
+    }
+
+
+
+
+    private Page randomPage(List<Page> liste) {
+        return liste.get(this.choix.nextInt(liste.size()));
+    }
+
+    private Page randomPageJoinable(List<Page> liste) throws NoJoinablePageException{
+        if (liste.size()>1) {
+            boolean found = false;
+            while (!found) {
+                Page choix = randomPage(liste);
+                if (!choix.getDeb()) {
+                    return choix; 
+                }
+            }
+        } else if (liste.size()==1) {
+            if (!liste.get(0).getDeb()){
+                return liste.get(0);
+            } else{
+                throw new NoJoinablePageException("La liste ne contient qu'un début");
+            }
+        } else{
+            throw new NoJoinablePageException("La liste est vide");
+        }
     }
 
 
